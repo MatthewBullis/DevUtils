723b4fae87a022f62d77e80e5fc4fdb18d88ced0
MatthewBullis <matthew.bullis@aura.com>
2022-10-16 14:57:19 -0500

    Create api_get_creds template

---
+++ src/utils/api_get_creds.template.py
@@ -0,0 +1,14 @@
+class Get_Creds():
+
+    def __init__(self):
+        self.idg_prod_api_key =  "Basic "
+        self.idg_prod_app_api_key = 'Basic '
+        self.suite_admin_userName = ""
+        self.suite_admin_pass = ""
+        self.suite_basic_auth = "Basic "
+        
+        self.idg_test_api_key =  "Basic "
+        self.idg_test_app_api_key = "Basic "
+        self.suite_test_admin_userName = ""
+        self.suite_test_admin_pass = ""
+        self.suite_test_basic_auth = "Basic "
